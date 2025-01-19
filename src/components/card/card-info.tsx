bf97068b157b6e7d428bfeeaccc63a2ae4c46b70
Kristoffer Robin Canlas <kristofferrobincanlas@gmail.com>
2025-01-19 22:51:57 +0800

    handle tap icon

--- src/components/card/card-info.tsx
+++ src/components/card/card-info.tsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import Grid from '@mui/material/Grid2';
 import useCardStore, { CardData } from "../../store/cardStore";
 import './card.css';
+import CardText from "./card-text";
 
 const CardInfo = () => {
   const card = useCardStore((state) => state.card);
@@ -10,7 +11,7 @@ const CardInfo = () => {
     <>
       <Box sx={{ width: '100%' }}>
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} columns={{ xs: 16, sm: 16, md: 16 }}>
-          <Grid size={{ xs: 16, sm: 16, md: 8 }}>
+          <Grid size={{ xs: 16, sm: 16, md: 8 }} sx={{ display: 'flex', justifyContent: 'center'}}>
           <img className="card-page-card-image" src={card?.imageUris.normal} alt={card?.name} />
           </Grid>
           <Grid className="card-page-card-info" size={{ xs: 16, sm: 16, md: 8 }}>
@@ -20,11 +21,11 @@ const CardInfo = () => {
             </div>
             <div className="row">
               <span className="label">Card Text:</span>
-              <span className="value" style={{whiteSpace: "pre-wrap"}}>{card?.oracleText}</span>
+              <span className="value" style={{whiteSpace: "pre-wrap"}}><CardText oracleText={card?.oracleText} /></span>
             </div>
             <div className="row">
               <span className="label">Rarity:</span>
-              <span className="value">{card?.rarity}</span>
+              <span className="value" style={{ textTransform: 'capitalize' }}>{card?.rarity}</span>
             </div>
             <div className="row">
               <span className="label">Artist:</span>
--- src/components/card/card-text.tsx
+++ src/components/card/card-text.tsx
@@ -1,12 +1,33 @@
-import Markdown from 'react-markdown'
+import React from "react";
+import tapLogo from "../../assets/tap-logo.svg";
 
 const CardText = ({
   oracleText
 }: { oracleText?: string }) => {
+
+  if (!oracleText) {
+    return (
+      <></>
+    )
+  }
+
+  const parts = oracleText.split(/{T}/);
+
   return (
-    <Markdown>
-      {oracleText}
-    </Markdown>
+    <>
+      {parts.map((part, index) => (
+        <React.Fragment key={index}>
+          {part}
+          {index < parts.length - 1 && (
+            <img
+              src={tapLogo}
+              alt="Tap Symbol"
+              style={{ width: '19px', height: '19px' }}
+            />
+          )}
+        </React.Fragment>
+      ))}
+    </>
   )
 }
 
