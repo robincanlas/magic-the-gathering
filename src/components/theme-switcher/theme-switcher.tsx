8b876d31fca38e73d004f2f514388cdee2a25419
Kristoffer Robin Canlas <kristofferrobincanlas@gmail.com>
2025-01-25 17:19:55 +0800

    fixed themeswitcher

--- src/components/theme-switcher/theme-switcher.tsx
+++ src/components/theme-switcher/theme-switcher.tsx
@@ -2,9 +2,16 @@ import Box from '@mui/material/Box';
 import { useColorScheme } from '@mui/material/styles';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
+import { useEffect } from 'react';
 
 const ThemeSwitcher = () => {
   const { mode, setMode } = useColorScheme();
+
+  useEffect(() => {
+    if (!mode) {
+      setMode('dark');
+    }
+  }, [mode]);
 
   if (!mode) {
     return null;
